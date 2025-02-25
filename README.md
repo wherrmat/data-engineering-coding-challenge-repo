1aa357f606dfa9b27453205561056fd8047c8b6d
wherrmat <wilmeryes96@yahoo.es>
2025-02-25 18:53:22 -0500

    ps1 file for creating storage account and sql database

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # data-engineering-coding-challenge-repo
 REST API for data migration and SQL analysis
+
+## Deployment
+1. Clone the repo using Azure PowerShell
+2. Specify the subscription name if required
+3. Specify a password for the database aligned with the requirements
---
+++ setup.ps1
@@ -0,0 +1,111 @@
+Clear-Host
+write-host "Starting deployment at $(Get-Date)"
+
+# parameters
+$resourceGroupName = "code-challenge-resource-group"
+$region = "East US"
+$storageAccountName = "ccstorageaccount"
+$sqlServerName = "ccsqlserver"
+$sqlDatabaseName = "ccsqldatabase"
+$sqlAdminLogin = "sqladmin"
+
+
+# Set azure subscription
+$subscriptions = Get-AzSubscription | Where-Object { $_.State -eq "Enabled" }
+
+if ($subscriptions.Count -eq 1) {
+    Write-Host "Subscription to be used for deployment: $($_.Name)"
+}
+else {
+    Write-Host "Available subscriptions" -ForegroundColor Green
+    for ($i = 0; $i -lt $subscriptions.Count; $i++) {
+        Write-Host "$($i + 1). $($subscriptions[$i].Name) (ID: $($subscriptions[$i].Id))"
+    }
+    $subscription_name = Read-Host "Please, type subscription name you want to use for deployment..."
+    Set-AzContext -Subscription $subscription_name
+}
+
+# Password for the database
+while ($complexPassword -ne 1)
+{
+    $SqlPassword = Read-Host "Enter a password to use for the $sqlUser login.
+    `The password must meet complexity requirements:
+    ` - Minimum 8 characters. 
+    ` - At least one upper case English letter [A-Z]
+    ` - At least one lower case English letter [a-z]
+    ` - At least one digit [0-9]
+    ` - At least one special character (!,@,#,%,^,&,$)
+    ` "
+
+    if(($SqlPassword -cmatch '[a-z]') -and ($SqlPassword -cmatch '[A-Z]') -and ($SqlPassword -match '\d') -and ($SqlPassword.length -ge 8) -and ($SqlPassword -match '!|@|#|%|\^|&|\$'))
+    {
+        $complexPassword = 1
+	    Write-Output "Password $SqlPassword accepted. Make sure you remember this!"
+    }
+    else
+    {
+        Write-Output "$SqlPassword does not meet the complexity requirements."
+    }
+}
+
+# Register resource providers
+Write-Host "Registering resource providers...";
+$provider_list = "Microsoft.Sql", "Microsoft.Storage"
+$maxRetries = 5
+$waittime = 30
+
+foreach ($provider in $provider_list) {
+    $retryCount = 0
+    while ($retryCount -lt $maxRetries) {
+        $currentStatus = (Get-AzResourceProvider -ProviderNamespace $provider).RegistrationState
+        if ($currentStatus -eq "Registered") {
+            Write-Host "$provider is successfully registered."
+            break
+        }
+        else {
+            Write-Host "$provider is not yet registered. Waiting for $waitTime seconds before rechecking..."
+            Start-Sleep -Seconds $waitTime
+            $retryCount++
+        }
+    }
+    if ($retryCount -eq $maxRetries) {
+        Write-Host "Failed to register $provider after $maxRetries attempts."
+    }
+}
+
+# Create resource group
+Write-Host "Creating $resourceGroupName resource group in $region ..."
+New-AzResourceGroup -Name $resourceGroupName -Location $region | Out-Null
+
+# Create storage account
+Write-Host "Creating $storageAccountName storage account ..."
+New-AzStorageAccount -ResourceGroupName $resourceGroupName `
+    -Name $storageAccountName `
+    -Location $region `
+    -SkuName Standard_LRS
+
+# Upload files
+write-host "Loading data..."
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $dataLakeAccountName
+$storageContext = $storageAccount.Context
+Get-ChildItem "./files/*.csv" -File | Foreach-Object {
+    write-host ""
+    $file = $_.Name
+    Write-Host $file
+    $blobPath = "csv/$file"
+    Set-AzStorageBlobContent -File $_.FullName -Container "files" -Blob $blobPath -Context $storageContext
+}
+
+# Create Azure SQL Server
+Write-Host "Creating $sqlServerName SQL server ..."
+New-AzSqlServer -ResourceGroupName $resourceGroupName `
+    -ServerName $sqlServerName `
+    -Location $region `
+    -SqlAdministratorCredentials (Get-Credential -UserName $sqlAdminLogin -Message "Ingrese la contraseña")
+
+# Create Azure SQL database
+Write-Host "Creating $sqlDatabaseName database in $sqlServerName SQL server ..."
+New-AzSqlDatabase -ResourceGroupName $resourceGroupName `
+    -ServerName $sqlServerName `
+    -DatabaseName $sqlDatabaseName `
+    -Edition Basic
