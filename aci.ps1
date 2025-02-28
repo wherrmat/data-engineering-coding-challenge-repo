7f74e787336d9e909c31861c947b0c00381daf88
wherrmat <wilmeryes96@yahoo.es>
2025-02-27 21:03:18 -0500

    update

--- aci.ps1
+++ aci.ps1
@@ -20,12 +20,5 @@ if ($subscriptions.Count -eq 1) {
     $subscriptionId = $subscriptions.Id
 }
 
-az keyvault create --resource-group $resourceGroupName --name $akv_name
-
-# Create service principal, store its password in AKV (the registry *password*)
-az keyvault secret set --vault-name $akv_name --name "$containerRegistryName-pull-pwd" --value $(az ad sp create-for-rbac --name "$containerRegistryName-pull" --scopes $(az acr show --name $containerRegistryName --query id --output tsv) --role acrpull --query password --output tsv)
-
-# Store service principal ID in AKV (the registry *username*)
-az keyvault secret set --vault-name $containerRegistryName --name $containerRegistryName-pull-usr --value $(az ad sp list --display-name $containerRegistryName-pull --query [].appId --output tsv)
-
-az container create --resource-group $resourceGroupName --name "acr-tasks" --image "$containerRegistryName.azurecr.io/$containerImageRegistryName" --registry-login-server $containerRegistryName.azurecr.io --registry-username $(az keyvault secret show --vault-name $akv_name --name "$containerRegistryName-pull-usr" --query value -o tsv) --registry-password $(az keyvault secret show --vault-name $akv_name --name "$containerRegistryName-pull-pwd" --query value -o tsv) --dns-name-label "acr-tasks-$containerRegistryName" --env DATABASE_ODBC_CONNECTION_STRING=$databaseStringConnection --query "{FQDN:ipAddress.fqdn}" --output table
+Connect-AzContainerRegistry
+az container create --resource-group $resourceGroupName --name "acr-tasks" --image "$containerRegistryName.azurecr.io/$containerImageRegistryName" --dns-name-label "acr-tasks-$containerRegistryName" --env DATABASE_ODBC_CONNECTION_STRING=$databaseStringConnection --query "{FQDN:ipAddress.fqdn}" --output table
